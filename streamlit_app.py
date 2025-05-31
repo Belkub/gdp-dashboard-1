5100b101c16e782cb2c01e1992250acd59dac2f4
Евгений <137326840+Belkub@users.noreply.github.com>
2025-05-31 17:31:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ im = Image.open("west_n.png")
 steps = st.slider('Количество точек', 150, 600)
 
 if st.button('Создать'):
-    im.show()
+    st.image("west_n.png", caption="Sunrise by the mountains")
     crop_rectangle = (150, 150, 400, steps)
     img = im.crop(crop_rectangle)
 

e3f7101cff395ac09e1bbd7d1f3f30942db8808a
Евгений <137326840+Belkub@users.noreply.github.com>
2025-05-31 18:15:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 from PIL import Image
 import streamlit as st
 
-im = Image.open("C:\\west\\west_n.png")
+im = Image.open("west_n.png")
 
 steps = st.slider('Количество точек', 150, 600)
 
 if st.button('Создать'):
-    st.image("C:\\west\\west_n.png", caption="Sunrise by the mountains")
-    ima = Image.open("C:\\west\\west_n.png")
+    st.image("west_n.png", caption="Sunrise by the mountains")
+    ima = Image.open("west_n.png")
     img = ima.crop((150, 150, 400, steps))  
     st.image(img)

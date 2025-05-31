ad199fa6b9cbdfdab518954c19365b2d0eb8f639
Евгений <137326840+Belkub@users.noreply.github.com>
2025-05-31 18:34:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import streamlit as st
-
+import plotly.express as px
 im = Image.open("west_n.png")
 
 steps = st.slider('Количество точек', 150, 600)
@@ -10,3 +10,6 @@ if st.button('Создать'):
     ima = Image.open("west_n.png")
     img = ima.crop((150, 150, 400, steps))  
     st.image(img)
+if st.button('graph'):
+    fig = px.scatter_3d(x=[1, 2, 3, 4], y=[4, 3, 2, 1], z=[1, 4, 2, 3])
+    st.plotly_chart(fig)
